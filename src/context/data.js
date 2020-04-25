db2f79cc51182d99951fe9b60b1520b6f52f6f65
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-04-25 15:37:09 +0200

    small update

--- src/context/data.js
+++ src/context/data.js
@@ -3,19 +3,25 @@ import img_project_moi from "../assets/img_project/vitrine_moi_v1.png"
 export const projects = [
   {
     name: "AutowriteJS",
-    description: "Open source javascript lightweight library for creating typing effect",
+    description: `
+      Open source javascript lightweight library for creating typing effect
+    `,
     img: "",
     tags: ["Javascript", "Open source", "Vanilla JS"]
   },
   {
     name: "DevJobLister",
-    description: "Job listing application for developpers, using the MERN stack",
+    description: `
+      Job listing application for developpers, using the MERN stack
+    `,
     img: "",
     tags: ["Javascript", "MERNstack", "React JS", "Node JS", "MongoDB"]
   },
   {
     name: "Maxime BOUNAAS-FERRET V1",
-    description: "Première version du site vitrine de Maxime BOUNAAS-FERRET, freelance en développement web",
+    description: `
+      Première version du site vitrine de Maxime BOUNAAS-FERRET, freelance en développement web
+    `,
     img: img_project_moi,
     tags: ["HTML5", "CSS3", "SASS", "Vanilla JS", "Adobe XD"]
   }
