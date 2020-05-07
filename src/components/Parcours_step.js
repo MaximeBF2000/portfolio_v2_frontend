13d8babe0eb792a37abc38d1cb74eebd34a34876
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-05-07 18:51:41 +0200

    added aos effect on parcours

--- src/components/Parcours_step.js
+++ src/components/Parcours_step.js
@@ -4,7 +4,7 @@ export default function Parcours_step({ step }) {
   const { date, title, description } = step
 
   return (
-    <li className="parcours_listItem">
+    <li className="parcours_listItem" data-aos="fade-up" data-aos-duration="1000">
       <div className="date"> {date} </div>
       <h3 className="title"> {title} </h3>
       <p className="description"> {description} </p>
