33bed3e7949ce79ceae130587aa7a58b9d9302f9
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-05-09 17:10:39 +0200

    try to disable spam filter in netlify form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,43 +1,7 @@
 import React from 'react'
-import NetlifyForm from 'react-netlify-form'
 
 export default function ContactForm() {
   return (
-    // <NetlifyForm name='Contact Form'>
-    //   {({ loading, error, success }) => (
-    //     <div>
-    //       {loading &&
-    //         <div>Loading...</div>
-    //       }
-    //       {error &&
-    //         <div>Your information was not sent. Please try again later.</div>
-    //       }
-    //       {success &&
-    //         <div>Thank you for contacting us!</div>
-    //       }
-    //       {!loading && !success &&
-    //         <div className="grid_el contact_form">
-    //           <div className="row">
-    //             <div className="input_group name">
-    //               <input type="text" name="name" className="input" required />
-    //               <label htmlFor="name" className="label">Nom</label>
-    //             </div>
-    //             <div className="input_group email">
-    //               <input type="text" name="email" className="input" required />
-    //               <label htmlFor="email" className="label">Email</label>
-    //             </div>
-    //           </div>
-    //   <div className="input_group message">
-    //     <textarea name="message" className="textarea" required></textarea>
-    //     <label htmlFor="message" className="label">Message</label>
-    //   </div>
-    //   <button type="submit" className="submitBtn"> Envoyer </button>
-    //         </div>
-    //       }
-    //     </div>
-    //   )}
-    // </NetlifyForm>
-
     <form action="https://maxime-bf.netlify.app" className="grid_el contact_form" method="post">
       <input type="hidden" name="form-name" value="contact" />
       <div className="row">
